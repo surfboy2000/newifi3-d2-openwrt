f0fb6acebff685be2ea71fd22a4e7282e1595aac
EVZYH <39973978+EVZYH@users.noreply.github.com>
2022-10-26 19:50:55 +0800

    1

---
+++ build-xiaomir2100.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export OP_BUILD_PATH=$PWD
+git clone -b openwrt-21.02 --single-branch https://github.com/immortalwrt/immortalwrt
+
+cd ${OP_BUILD_PATH}/immortalwrt
+./scripts/feeds update -a && ./scripts/feeds install -a
+rm -rf ./tmp && rm -rf .config
+mv ${OP_BUILD_PATH}/xiaomir2100.config ${OP_BUILD_PATH}/immortalwrt/.config
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+make defconfig
+make download -j8
+make V=s -j$(nproc)
+echo "FILE_DATE=$(date +%Y%m%d%H%M)" >>$GITHUB_ENV
